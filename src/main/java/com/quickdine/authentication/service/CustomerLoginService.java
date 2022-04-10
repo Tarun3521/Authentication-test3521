6ffdfc26a740f4f72361e9280d7259857cfdd290
Tarun3521 <tarunkishor1@gmail.com>
2022-04-10 16:58:43 +0530

    Update CustomerLoginService.java

--- src/main/java/com/quickdine/authentication/service/CustomerLoginService.java
+++ src/main/java/com/quickdine/authentication/service/CustomerLoginService.java
@@ -17,14 +17,19 @@ public class CustomerLoginService {
     private CustomerLoginRepository customerLoginRepository;
 
 
-    public CustomerInfo saveUserDetails(OAuth2User user) {
+    public void saveUserDetails(OAuth2User user) {
         Map<String, Object> customerDetails = user.getAttributes();
         CustomerInfo customerInfo = new CustomerInfo();
         customerInfo.setCustomerFirstName(String.valueOf(customerDetails.get("family_name")));
         customerInfo.setCustomerLastName(String.valueOf(customerDetails.get("given_name")));
         customerInfo.setEmailId(String.valueOf(customerDetails.get("email")));
         customerInfo.setProvider(Provider.GMAIL);
-        return customerLoginRepository.save(customerInfo);
+        List<CustomerInfo> emails=customerLoginRepository.findAll();
+       for(CustomerInfo custEmails:emails){
+           if(!customerInfo.getEmailId().equals(custEmails.getEmailId())){
+               customerLoginRepository.save(customerInfo);
+           }
+       }
     }
 
     public List<CustomerInfo> getCustomerDetails() {
